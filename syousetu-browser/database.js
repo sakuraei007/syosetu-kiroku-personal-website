a1b9f3ff2a2ccc30b3ff01d4c1f1fc74d8780fba
Eitaro Sakurayama <eitaros2@illinois.edu>
2026-07-02 10:11:46 +0900

    newdatabase tursor

--- syousetu-browser/database.js
+++ syousetu-browser/database.js
@@ -1,32 +1,35 @@
 // database.js
-const Database = require('better-sqlite3');
+const { createClient } = require('@libsql/client');
+require('dotenv').config();
 
-const database = new Database('novels.db');
+const database = createClient({
+  url: process.env.TURSO_URL,
+  authToken: process.env.TURSO_TOKEN,
+});
 
-// user database table
-database.exec(`
-  CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
-  )
-`);
+async function initDatabase() {
+  await database.execute(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      username TEXT NOT NULL UNIQUE,
+      password TEXT NOT NULL
+    )
+  `);
 
-// novel database table 
-database.exec(`
-  CREATE TABLE IF NOT EXISTS novels (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    title TEXT NOT NULL,
-    url TEXT NOT NULL,
-    tags TEXT NOT NULL,
-    summary TEXT,
-    rating INTEGER DEFAULT 0,
-    status TEXT DEFAULT '完結済',
-    user_id INTEGER
-  )
-`);
+  await database.execute(`
+    CREATE TABLE IF NOT EXISTS novels (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      title TEXT NOT NULL,
+      url TEXT NOT NULL,
+      tags TEXT NOT NULL,
+      summary TEXT,
+      rating INTEGER DEFAULT 0,
+      status TEXT DEFAULT '完結済',
+      user_id INTEGER
+    )
+  `);
+}
 
-
-
+initDatabase().catch(console.error);
 
 module.exports = database;
